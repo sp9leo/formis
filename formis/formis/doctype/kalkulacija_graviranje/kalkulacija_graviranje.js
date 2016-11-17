4e756f0d28559f41ab6544935aa5ea71716f2e1a
sp9leo <sp9leonardo@gmail.com>
2016-11-17 20:36:24 +0100

    update
    dodani scripti

--- formis/formis/doctype/kalkulacija_graviranje/kalkulacija_graviranje.js
+++ formis/formis/doctype/kalkulacija_graviranje/kalkulacija_graviranje.js
@@ -6,3 +6,50 @@ frappe.ui.form.on('Kalkulacija graviranje', {
 
 	}
 });
+frappe.ui.form.on("Kalkulacija graviranje", "len", function(frm) {
+cur_frm.set_value("area", (frm.doc.len* frm.doc.wid));
+ });
+ 
+ frappe.ui.form.on("Kalkulacija graviranje", "wid", function(frm) {
+ cur_frm.set_value("area", (frm.doc.len* frm.doc.wid))       
+ });
+ 
+ //izracunaj povrsino//
+ frappe.ui.form.on("Kalkulacija graviranje", "area", function(frm) {
+ cur_frm.set_value("mm2", (frm.doc.area*100));
+ cur_frm.set_value("m2", (frm.doc.area/10000));
+ if (frm.doc.area > 25) {cur_frm.set_value("areaovermin", (frm.doc.area-25))}else {frm.set_value("areaovermin",00)}
+     
+ });
+ 
+ //izracunaj znesek glede na povrsino//
+ frappe.ui.form.on("Kalkulacija graviranje", "refresh", function(frm) {
+ var d= (frm.doc.variable_price*frm.doc.areaovermin);
+if (frm.doc.area <= 25 && frm.doc.qty == 1){ frm.set_value("value", frm.doc.current_price); } 
+ else {frm.set_value("value",d+frm.doc.current_price);}
+cur_frm.set_value("valueovermin",d);
+ });
+ 
+ //izracunaj ceno glede na kolicino//
+ frappe.ui.form.on("Kalkulacija graviranje", "qty", function(frm) {
+ var qty= frm.doc.qty;   
+ if (qty>1 && qty<= 10) {frm.set_value("current_price",frm.doc.p1);}
+ else if (qty>10 && qty<=25) {frm.set_value("current_price",frm.doc.p2);}
+ else if (qty>25 && qty<=50) {frm.set_value("current_price",frm.doc.p3);}
+ else if (qty>50 && qty<=99) {frm.set_value("current_price",frm.doc.p4);}
+ else if (qty>99) {msgprint("cena po dogovoru");}
+ else {frm.set_value("current_price",frm.doc.fixed_price);}
+ frm.set_value("total_value",frm.doc.value*frm.doc.qty);
+frm.refresh();
+ });
+
+frappe.ui.form.on("Kalkulacija graviranje", "refresh", function(frm) {
+  frm.set_value("total_value",frm.doc.value*frm.doc.qty);
+ });
+ 
+ frappe.ui.form.on("Kalkulacija graviranje", "refresh", function(frm) {
+    frm.add_custom_button(__("Do Something"), function() {
+      //msgprint("hello");
+frm.refresh();
+ });
+	});
