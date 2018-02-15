af1cb8e39b982c58b6970d9c5f2cea5f79b402bf
sp9leo <sp9leonardo@gmail.com>
2018-02-15 09:46:13 +0100

    Update hooks.py
    deleted custom fields from fixtures

--- formis/hooks.py
+++ formis/hooks.py
@@ -10,7 +10,7 @@ app_color = "grey"
 app_email = "formis"
 app_version = "0.0.2"
 app_license = "MIT"
-fixtures = ["Custom Field", "Custom Script", "Print Format", "Workflow"]
+fixtures = ["Custom Script", "Print Format", "Workflow"]
 
 # Includes in <head>
 # ------------------
