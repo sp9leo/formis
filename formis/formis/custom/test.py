b331c3dd0085c7d26a3785a115b258c9f182f280
sp9leo <sp9leonardo@gmail.com>
2018-08-16 17:36:17 +0200

    Update test.py

--- formis/formis/custom/test.py
+++ formis/formis/custom/test.py
@@ -1,1 +1,20 @@
 @frappe.whitelist()
+def testnafunkcija:
+frappe.call({
+					"method": "frappe.client.set_value",
+					"args": {
+						//replace "Target DocType" with the actual target doctype
+						"doctype": "Delovni nalog",
+						//replace target_doctype_link with a link to the document to be changed
+						"name": frm.doc.Delovni_nalog,
+						"fieldname": {
+							//You can update as many fields as you want.  
+							"stranka": frm.doc.customer,
+							"datum": frm.doc.date,
+							"target_field_3": frm.doc.source_field_3,
+							"target_field_4": frm.doc.source_field_4,
+							"target_field_5": frm.doc.source_field_5  //Make sure that you do not put a comma over the last value
+						},
+					}
+				});
+	
